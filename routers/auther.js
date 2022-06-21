2e2861178c709b6301d7a37fb32465c22a3438a2
adil tahiri <rajaadil1995@gmail.com>
2022-06-21 19:29:30 +0100

    create post method

--- routers/auther.js
+++ routers/auther.js
@@ -1,3 +1,22 @@
 const express = require("express");
 const router = express.Router();
+
 const Auth = require("../models/Auth");
+
+router.get("/register", (req, res) => {
+  res.send("hi");
+});
+router.post("/register", async (req, res) => {
+  const user = await new Auth({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  try {
+    res.json(user);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+});
+
+module.exports = router;
