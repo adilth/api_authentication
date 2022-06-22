f6fdc5d5714eed5bcb07a2b8a748142a510ef933
adil tahiri <rajaadil1995@gmail.com>
2022-06-22 02:37:28 +0100

    add new filed to server

--- server.js
+++ server.js
@@ -3,20 +3,21 @@ const app = express();
 const mongoose = require("mongoose");
 
 require("dotenv").config();
-
+//import routers
+const autherRouter = require("./routers/auther");
+const postsRouter = require("./routers/Posts");
 const PORT = process.env.PORT || 3000;
 
 //connect to DB
 
 mongoose.connect(process.env.CONNECT_DB, () => console.log("Connect to DB"));
 
-//import routers
-const autherRouter = require("./routers/auther");
-
 //router middleware
+// app.use(express.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
+app.use(express.json());
 app.set("view engine", "ejs");
 app.use("/api/user", autherRouter);
-app.use(express.json());
+app.use("/api/posts", postsRouter);
 
 //listent
 app.listen(PORT, () => console.log("it runnig"));
