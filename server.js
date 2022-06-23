0e98b103b7590920f7a55b9257dc83a9b07c9a44
adil tahiri <rajaadil1995@gmail.com>
2022-06-23 01:34:59 +0100

    add cors middleware

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 require("dotenv").config();
 //import routers
@@ -16,8 +17,19 @@ mongoose.connect(process.env.CONNECT_DB, () => console.log("Connect to DB"));
 // app.use(express.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
 app.use(express.json());
 app.set("view engine", "ejs");
-app.use("/api/user", autherRouter);
-app.use("/api/posts", postsRouter);
+app.use(express.static("public"));
+// Middleware to accept requests from localhost:3000
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use("/user", autherRouter);
+app.use("/posts", postsRouter);
 
+// app.get("/register", (req, res) => {
+//   res.render("register");
+// });
 //listent
 app.listen(PORT, () => console.log("it runnig"));
