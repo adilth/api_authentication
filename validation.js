7750e1af5c64d462e10ce7f2124df3709b33fd27
adil tahiri <rajaadil1995@gmail.com>
2022-06-24 02:34:53 +0100

    change some validation

--- validation.js
+++ validation.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const registerValidation = (data) => {
   const schema = Joi.object({
     name: Joi.string().min(6).required(),
-    email: Joi.string().min(8).required().email(),
+    email: Joi.string().min(6).required().email(),
     password: Joi.string().min(8).required(),
   });
   return schema.validate(data);
