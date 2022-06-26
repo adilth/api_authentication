927bc672a1ec216111b20e161c79117a0c79291d
adil tahiri <rajaadil1995@gmail.com>
2022-06-26 02:16:31 +0100

    add authenticare to home and welcome

--- routers/home.js
+++ routers/home.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-
+const { ensureAuthenticated, forwardAuthenticated } = require("./isauth");
 const verify = require("./verifyToken");
 
-router.get("/", verify, (req, res) => {
+router.get("/", verify, ensureAuthenticated, (req, res) => {
   res.render("index.ejs");
 });
-router.get("/welcome", (req, res) => {
+router.get("/welcome", forwardAuthenticated, (req, res) => {
   res.render("welcome.ejs");
 });
 module.exports = router;
