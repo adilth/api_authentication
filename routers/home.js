15f70c9d82b4cdaa07ec2ef87c1783b92b4b894c
adil tahiri <rajaadil1995@gmail.com>
2022-07-03 02:13:57 +0100

    render to home

--- routers/home.js
+++ routers/home.js
@@ -5,6 +5,7 @@ const verify = require("./verifyToken");
 
 router.get("/", verify, ensureAuthenticated, (req, res) => {
   res.render("index.ejs");
+  console.log(req);
 });
 router.get("/welcome", forwardAuthenticated, (req, res) => {
   res.render("welcome.ejs");
