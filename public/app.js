d30d4e70477751bcd784c136f27c79f4f773de9a
adil tahiri <rajaadil1995@gmail.com>
2022-07-03 02:11:25 +0100

    add fetch login

--- public/app.js
+++ public/app.js
@@ -1,7 +1,8 @@
 let password = document.getElementById("password").value;
-let nameInput = document.getElementById("name").value;
+// let nameInput = document.getElementById("name").value;
 let worning = document.getElementById("worning");
-document.querySelector(".btn").addEventListener("click", validate);
+// document.querySelector(".btn").addEventListener("click", validate);
+document.querySelector("#login").addEventListener("click", makeRequest);
 // let worningName = document.getElementById("wouning");
 let passw = /^[A-Za-z]\w{7,14}$/;
 
@@ -16,3 +17,25 @@ let passw = /^[A-Za-z]\w{7,14}$/;
 //     document.querySelector(".btn").submit();
 //   }
 // }
+
+function makeRequest() {
+  fetch("/user/login", {
+    method: "POST", // or 'PUT'
+    body: JSON.stringify(data), // data can be `string` or {object}!
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((response) => console.log("Success:", JSON.stringify(response)))
+    .catch((error) => console.error("Error:", error));
+  //  .then((res) => {
+  //    if (res.status == 200) {
+  //      return res.text();
+  //    } else {
+  //      throw Error(res.statusText);
+  //    }
+  //  })
+  //  .then((responseText) => logResponse("requestResponse", responseText))
+  //  .catch(console.error);
+}
