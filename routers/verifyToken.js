cb11ca1d67d263d7390dcd7b3ee1d148ec9eae68
adil tahiri <rajaadil1995@gmail.com>
2022-07-04 01:20:57 +0100

    verify the token

--- routers/verifyToken.js
+++ routers/verifyToken.js
@@ -6,9 +6,22 @@ const verify = function (req, res, next) {
   if (!token) return res.status(401).json({ message: "Invalid token" });
 
   try {
-    const varified = jwt.verify(token, process.env.JWT_SECRT);
-    req.user = varified;
-    next();
+    const varified = jwt.verify(
+      token,
+      process.env.JWT_SECRT,
+      function (err, decoded) {
+        if (err)
+          return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+        req.user = varified;
+        req.token = token;
+        // res.status(200).send(user); Comment this out!
+        next(user); // add this line
+      }
+    );
+
+    // res.status(200).send(token);
+    // next();
   } catch (err) {
     return res.status(401).json({ message: err.message });
   }
