4a7a731eef559c7301c5ada3df1f089c9f9147a7
adil tahiri <rajaadil1995@gmail.com>
2022-07-05 02:03:35 +0100

    add make it async

--- routers/verifyToken.js
+++ routers/verifyToken.js
@@ -1,24 +1,27 @@
 const jwt = require("jsonwebtoken");
 
-const verify = function (req, res, next) {
-  const tokenSecret = req.header["auth-token"];
-  const token = req.cookies.token || (tokenSecret && tokenSecret.split(" ")[1]);
+const verify = async function (req, res, next) {
+  const tokenSecret = req.header["x-auth-token"];
+  const token =
+    req.cookies.token ||
+    req.cookies.access_token ||
+    tokenSecret ||
+    tokenSecret.split(" ")[1] ||
+    req.body.token;
   if (!token) return res.status(401).json({ message: "Invalid token" });
 
   try {
-    const varified = jwt.verify(
-      token,
-      process.env.JWT_SECRT,
-      function (err, decoded) {
-        if (err)
-          return res.status(500).send("There was a problem finding the user.");
-        if (!user) return res.status(404).send("No user found.");
-        req.user = varified;
-        req.token = token;
-        // res.status(200).send(user); Comment this out!
-        next(user); // add this line
-      }
-    );
+    const varified = await jwt.verify(token, process.env.JWT_SECRT);
+    // function (err, decoded) {
+    //   if (err)
+    //     return res.status(500).send("There was a problem finding the user.");
+    //   if (!user) return res.status(404).send("No user found.");
+    req.user = varified;
+    req.token = token;
+    // res.status(200).send(user); Comment this out!
+    return next(); // add this line
+    // }
+    // );
 
     // res.status(200).send(token);
     // next();
