b5444fa9459792df4af54aa58f18a501fe599aeb
adil tahiri <rajaadil1995@gmail.com>
2022-07-07 18:26:59 +0100

    add token to regester

--- routers/auther.js
+++ routers/auther.js
@@ -7,6 +7,7 @@ const { registerValidation, loginValidation } = require("../validation");
 const { forwardAuthenticated } = require("./isauth");
 
 const Auth = require("../models/Auth");
+const { response } = require("express");
 
 router.get("/register", forwardAuthenticated, (req, res) => {
   res.render("register.ejs");
@@ -30,17 +31,13 @@ router.post("/register", async (req, res) => {
     res.render("register", { error: message });
   }
   const emailExists = await Auth.findOne({ email: req.body.email });
+  //check if the user is already registered
   if (emailExists) {
     const message = "User already registered.";
     // errorMsg.push({ msg: message });
     res.render("register", { error: message });
     return;
   }
-  //check if the user is already registered
-
-  // if (error) {
-  //   res.render("register", { error });
-  // }
 
   try {
     //hah passwords
@@ -53,6 +50,18 @@ router.post("/register", async (req, res) => {
       email: req.body.email,
       password: hashPassword,
     });
+
+    // Create token
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.JWT_SECRT,
+      {
+        expiresIn: 86400,
+      }
+    );
+
+    // save user token
+    user.token = token;
 
     const saveUser = await user.save();
     req.flash(
@@ -68,90 +77,91 @@ router.post("/register", async (req, res) => {
 });
 
 router.post("/login", (req, res, next) => {
-  passport.authenticate(
-    "local",
-    {
-      successRedirect: "/home",
-      failureRedirect: "/user/login",
-      failureFlash: true,
-    },
-    async (err, users, info) => {
-      //validate the data before user
-      const { error } = loginValidation(req.body);
-      if (err) {
-        const { details } = error;
-        console.log(err);
-        const message = details.map((i) => i.message).join(",");
-        console.log("error", message);
-        // errorMsg.push({ msg: message });
-        // res.status(422).json({ message });
+  passport.authenticate("local", async (err, users, info) => {
+    //validate the data before user
+    const { error } = loginValidation(req.body);
+    if (err) {
+      const { details } = error;
+      console.log(err);
+      const message = details.map((i) => i.message).join(",");
+      console.log("error", message);
+      // errorMsg.push({ msg: message });
+      // res.status(422).json({ message });
+      res.render("login", { error: message });
+      // res.status(404).send({ message: error.message });
+      return;
+    }
+
+    //check if the user is already registered
+
+    // if (!user) return res.status(404).json({ message: "Email not found" });
+
+    // const validPass = await bcrypt.compare(req.body.password, user.password);
+    // if (!validPass)
+    //   return res.status(400).json({ message: "Password is incorrect" });
+
+    //create and assign a token
+    try {
+      if (info != undefined) {
+        console.log(info.message);
+        const { message } = info;
         res.render("login", { error: message });
-        // res.status(404).send({ message: error.message });
+        console.log(message);
         return;
-      }
+      } else {
+        const user = await Auth.findOne({ email: req.body.email });
+        const token = jwt.sign({ _id: user.id }, process.env.JWT_SECRT, {
+          expiresIn: "1h",
+        });
+        req.logIn(user, { session: false }, function (err) {
+          // Should not cause any errors
 
-      // if (error) {
-      // return req.flash("errorLogin", `${error.details[0].message}`);
-      // var err = new Error(error.details[0].message);
-      // err.status = 400;
-      // return next(err);
-      // }
-
-      //check if the user is already registered
-
-      // if (!user) return res.status(404).json({ message: "Email not found" });
-
-      // const validPass = await bcrypt.compare(req.body.password, user.password);
-      // if (!validPass)
-      //   return res.status(400).json({ message: "Password is incorrect" });
-
-      //create and assign a token
-      try {
-        if (info != undefined) {
-          console.log(info.message);
-          const { message } = info;
-          res.render("login", { error: message });
-          console.log(message);
-          return;
-        } else {
-          const user = await Auth.findOne({ email: req.body.email });
-          const token = jwt.sign({ _id: user.id }, process.env.JWT_SECRT, {
-            expiresIn: "1h",
-          });
+          if (err) {
+            return next(err);
+          }
           res.cookie("token", token, { httpOnly: true });
           res.header("x-auth-token", token);
-          //.send({
-          //   success: true,
-          //   token: token,
-          //   message: "logged in",
-          // });
-          // res.status(200).data = {
-          //   user: user,
-          //   token,
-          // };
-          return res.redirect("/home");
-          // res.redirect("/home" + token);
-          // res.status(200).send({
-          //   auth: true,
-          //   token: token,
-          //   data: { _id: user.id },
-          //   message: "user found & logged in",
-          // });
-        }
-      } catch (err) {
-        console.log(err);
-        res.status(404).json({ messages: err.message });
+          res.redirect("/home");
+          // res.send(user);
+        });
+
+        // .send(JSON.stringify(info));
+        // res.status(200).data = {
+        //  auth: true,
+        //   user: user,
+        //   token,
+        // };
+        // return res.redirect("/home");
       }
+    } catch (err) {
+      console.log(err);
+      res.status(404).json({ messages: err.message });
     }
-  )(req, res, next);
+  })(req, res, next);
 });
-router.get("/logout", (req, res) => {
-  // req.logout();
-  req.flash("success", "You are logged out");
-  req.session.destroy(() => {
-    res.clearCookie("connect.sid");
+router.get("/logout", (req, res, next) => {
+  res.clearCookie("user");
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "You are logged out");
+    req.session.destroy();
     res.redirect("/home/welcome");
   });
 });
-
+// DELETE /api/auth/logout
+// router.delete("/logout", (req, res) => {
+//   if (req.session) {
+//     req.session.destroy((err) => {
+//       if (err) {
+//         res.status(400).send("Unable to log out");
+//       } else {
+//         res.send("Logout successful");
+//       }
+//     });
+//   } else {
+//     res.end();
+//   }
+// });
 module.exports = router;
